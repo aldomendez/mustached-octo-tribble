61ded6a39e2c390da79fb36a79be723453287a54
Aldo Mendez <aldomendez86@gmail.com>
2013-12-29 12:49:41 -0600

    Completados todas las funciones que ingresan datos a las bases de datos
    Todas funcionan bien y tienen sus respectivos querys

--- formHandler.php
+++ formHandler.php
@@ -27,11 +27,16 @@ function bind_id($file)
 {
 	// Toma el valor maximo de la base de datos y lo devuelve como ID
 	global $db;
+	// Tomamos el valor de la tabla de acuerdo a los procesos que corremos
 	$tabla = array(
 		'ICRX2' => 'shear_icrx2',
-		'SuperNovaROSA_HIC' => 'supernovarosa_hic'
+		'SuperNovaROSA_HIC' => 'supernovarosa_hic',
+		'PMQPSK' => 'shear_pmkpsk',
+		'DiamondROSA_HIC' => 'DiamondROSA_HIC'
 	);
 	$db->query('select max(id) id from ' . $tabla[$_POST['process']]);
+	// $db->results[0]['ID']  << Este valor representa de la lista de valores que retorna el query
+	//                           el primer valor de la lista ID
 	return str_replace(':id', $db->results[0]['ID'] + 2 , $file);
 }
 
@@ -83,6 +88,65 @@ function SuperNovaROSA_HIC()
 	$file = bind($file,'Capacitor_3');
 	$file = bind($file,'Capacitor_4');
 	$file = bind($file,'Capacitor_5');
+
+
+	// Despues de sustituir todas las variables,
+	// insertamos en la base de datos
+	$db->insert($file);
+}
+
+function PMQPSK()
+{
+	// Hace visible el valor $db dentro de la funcion
+	global $db;
+	$file = file_get_contents('sql/insert_PMQPSK.sql');
+
+	// Sustituimos todas las variables
+	$file = bind_id($file);
+	$file = bind($file, 'process');
+	$file = bind($file, 'serial_num');
+	$file = bind($file, 'system_id');
+	$file = bind($file, 'user_id');
+	$file = bind($file, 'comments');
+	$file = bind($file, 'passfail');
+    $file = bind($file, 'Diodo_1');
+    $file = bind($file, 'Diodo_2');
+    $file = bind($file, 'Diodo_3');
+    $file = bind($file, 'Diodo_4');
+
+
+	// Despues de sustituir todas las variables,
+	// insertamos en la base de datos
+	$db->insert($file);
+}
+
+function DiamondROSA_HIC()
+{
+	// Hace visible el valor $db dentro de la funcion
+	global $db;
+	$file = file_get_contents('sql/insert_DiamondROSA_HIC.sql');
+
+	// Sustituimos todas las variables
+	$file = bind_id($file);
+	$file = bind($file, 'process');
+	$file = bind($file, 'serial_num');
+	$file = bind($file, 'system_id');
+	$file = bind($file, 'user_id');
+	$file = bind($file, 'comments');
+	$file = bind($file, 'passfail');
+    $file = bind($file, 'TIA_1');
+    $file = bind($file, 'TIA_2');
+    $file = bind($file, 'TIA_3');
+    $file = bind($file, 'TIA_4');
+    $file = bind($file, 'Capacitor_1');
+    $file = bind($file, 'Capacitor_2');
+    $file = bind($file, 'Capacitor_3');
+    $file = bind($file, 'Capacitor_4');
+    $file = bind($file, 'Capacitor_5');
+    $file = bind($file, 'Capacitor_6');
+    $file = bind($file, 'Capacitor_7');
+    $file = bind($file, 'Capacitor_8');
+    $file = bind($file, 'Capacitor_SMT');
 
 
 	// Despues de sustituir todas las variables,
